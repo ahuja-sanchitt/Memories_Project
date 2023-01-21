6e6776d2ee68f97cf5b552e4a69802ffaa5e0171
ahuja-sanchitt <sanchitahujafas@gmail.com>
2023-01-21 18:58:58 +0530

    adding frontend resources

---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,21 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (posts =[], action) => {
+    
+    // eslint-disable-next-line no-undef
+    switch(action.type){
+                case DELETE:
+                    return posts.filter((post)=> post._id !==action.payload);
+                case UPDATE:
+                    return posts.map((post) =>post._id===action.payload._id? action.payload : post)
+        
+                case FETCH_ALL:
+                    return action.payload;
+                case CREATE:
+                    return [...posts,action.payload];
+                default:
+                return posts;
+    }
+}
+
--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/posts', postRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => app.listen( PORT, () => console.log('Server Running on port: ${PORT} ')))
+.then ( () => app.listen( PORT, () => console.log(`Server Running on port: ${PORT} `)))
 .catch( (error) => console.log(error.message))
 
 
